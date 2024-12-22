e520d38a47f3e7a9c6fae01a163bc07da40b3371
Prashant3-playo <prashant@playo.co>
2024-12-22 06:23:21 +0530

    first commit

---
+++ Custom Trasitions Between Screens/Custom_Trasitions_Between_ScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Trasitions_Between_ScreensApp.swift
+//  Custom Trasitions Between Screens
+//
+//  Created by Prashant Kumar Soni on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Trasitions_Between_ScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
